f677a6098db67e7426b05029bfef3458e34d5812
Michael Völkel <eisflamme@mihahome.de>
2022-11-21 15:41:02 +0000

    better example

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <QApplication>
 #include <QMainWindow>
+#include <QLabel>
+#include <QPushButton>
+#include <QBoxLayout>
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+
     QMainWindow mainWin;
     mainWin.resize(400, 400);
+    mainWin.move(200, 200);
+
+    auto l = new QHBoxLayout(&mainWin);
+
+    auto label = new QLabel("Hallo", &mainWin);
+    auto btn = new QPushButton("Button", &mainWin);
+
+    l->addWidget(label);
+    l->addWidget(btn);
+
     mainWin.show();
 
     return app.exec();
